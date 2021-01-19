f7c3b37c36a5a1cce52c5448e817ccc6b75aa53b
Edison Garcia <egachi@live.com>
2021-01-19 17:36:17 -0600

    Intial Commit

---
+++ index.py
@@ -0,0 +1,27 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('data.csv', index_col=0, parse_dates=True)
+fig = px.bar(df, x="cars", y="values", color="real")
+
+app.layout = html.Div(children=[
+    html.H1(children='Car Prices'),
+    html.Div(children='''
+        - Real vs Values -
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
